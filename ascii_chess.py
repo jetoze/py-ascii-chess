86615fa37c80a6a8fd85128edbd5bdd03a5ff414
tzeth <jetoze@gmail.com>
2016-10-13 11:52:53 -0700

    Added save command, for saving the list of moves to a file.

--- ascii_chess.py
+++ ascii_chess.py
@@ -692,6 +692,7 @@ class Game:
 	def __init__(self):
 		self._board = Board.initial_position()
 		self._half_move = 1
+		self._move_list = []
 
 	def loop(self):
 		print
@@ -699,7 +700,7 @@ class Game:
 		while True:
 			try:
 				input = raw_input("\nEnter a move for {} ('q' to quit, 'b' to print board): ".format(self.side_to_move()))
-				self.handle_input(input)
+				self.handle_input(input.strip())
 			except ValueError as ve:
 				print str(ve)
 			except InvalidMoveError as ime:
@@ -719,11 +720,16 @@ class Game:
 					file_name = raw_input("Enter file name: ")
 					if len(file_name):
 						self.load_file(file_name)
+			elif input == 'save':
+					file_name = raw_input("Enter file name: ")
+					if len(file_name):
+						self.save_moves_to_file(file_name)
 			else:
 				expected_color = self.side_to_move()
 				move = self._board.parse_move(input, expected_color)
 				move.update_board(self._board, expected_color)
 				self._half_move += 1
+				self._move_list.append(input)
 				if print_board:
 					self._board.dump()
 
@@ -731,17 +737,30 @@ class Game:
 		try:
 			with open(file_name) as f:
 				for line in (ln.strip() for ln in f if len(ln)):
-					print line,
-					if " " in line:
-						moves = line.split(" ")
-						self.handle_input(moves[0], False, False)
+					print line
+					moves = line.split()
+					self.handle_input(moves[0], False, False)
+					if len(moves) > 1:
 						self.handle_input(moves[1], False, False)
-					else:
-						self.handle_input(line, False, False)
 			print '\n'
 			self._board.dump()
 		except IOError as err:
 			print err
+
+	def save_moves_to_file(self, file_name):
+		try:
+			with open(file_name, "w") as f:
+				n = 0
+				for mv in self._move_list:
+					n += 1
+					f.write(mv)
+					if (n % 2):
+						f.write("\t")
+					else:
+						f.write("\n")
+		except IOError as err:
+			print err
+
 
 	def side_to_move(self):
 		"""Returns WHITE or BLACK depending on which side is to move."""
